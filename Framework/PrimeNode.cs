93bf2e4e8c42ca03078c713089ecee1463523516
eiyukabe <eiyukabe@gmail.com>
2019-12-18 23:30:13 -0500

    PrimeNode (similar to PrimeNode2D, but extends Node).

---
+++ Framework/PrimeNode.cs
@@ -0,0 +1,36 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public class PrimeNode : Node
+{
+    public List<T> GetChildren<T>() where T : Node
+    {
+        var results = new List<T>();
+
+        foreach (Node node in GetChildren())
+        {
+            if (node is T)
+            {
+                results.Add((T)node);
+            }
+        }
+
+        return results;
+    }
+
+    public T GetAncestorOfType<T>() where T : Node
+    {
+        var ancestor = GetParent();
+        while (ancestor != null)
+        {
+            if (ancestor is T)
+            {
+                return (T)ancestor;
+            }
+            ancestor = ancestor.GetParent();
+        }
+        return null;
+    }
+
+}
