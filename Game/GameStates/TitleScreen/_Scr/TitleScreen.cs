d31b9aec8c904b0fbdb2342f6cf4bc6e3d9bcd26
Brian <veluzy@gmail.com>
2019-12-21 06:01:48 +0800

    Added TitleScreen template to Game

---
+++ Game/GameStates/TitleScreen/_Scr/TitleScreen.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System;
+
+public class TitleScreen : GameState
+{
+    public override void _Input(InputEvent ev)
+    {
+        if (Input.IsActionJustPressed(InputActions.CANCEL))
+        {
+            GetTree().Quit();
+        }
+    }
+
+
+    #region Button Callbacks
+
+    private void OnPlayButtonPressed()
+    {
+        SceneManager.ChangeScene("res://Game/GameStates/Play/Play.tscn", GetTree());
+    }
+
+    private void OnQuitButtonPressed()
+    {
+        GetTree().Quit();
+    }
+
+    #endregion
+}
