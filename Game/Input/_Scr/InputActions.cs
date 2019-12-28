b59f9cdeafdb6989fd2b78c1d8cb5cb1e8c353cc
Brian <veluzy@gmail.com>
2019-12-29 06:10:47 +0800

    Moved InputActions to Constants folder (not sure about this decision)

--- Game/Input/_Scr/InputActions.cs
+++
@@ -1,9 +0,0 @@
-using Godot;
-using System;
-
-/// <summary> A list of constant strings to be used as action names. Use these constants when binding keys to actions and when checking for player input,
-///           such as with Input.IsActionPressed(). See also Bind class. </summary>
-public static partial class InputActions
-{
-    
-}
