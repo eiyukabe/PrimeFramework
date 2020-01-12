6b21ae351bf5920f3c57241253b95d0e8b75028f
eiyukabe <eiyukabe@gmail.com>
2020-01-11 23:29:52 -0500

    More event work for the agency system.

--- Framework/AI/Agency/Event.cs
+++ Framework/AI/Agency/Event.cs
@@ -1,18 +1,66 @@
 using Godot;
 using System;
 
-///
+/// A special type of behavior that is fired off when something external to the agency occurs (e.g. the agent dying).
 public abstract class Event : Behavior
 {
 
-    public override void OnBegin()
-    {
-        
-    }
+    #region Initialization
 
-    public override void InactiveProcess(float Delta)
-    {
-        base.InactiveProcess(Delta);
-    }
+        public override void OnBegin()
+        {
+            foreach (Behavior Child in ChildBehaviors)
+            {
+                Child.Begin();
+            }
+        }
+
+    #endregion
+
+
+    #region Control
+
+        public override void InactiveProcess(float delta)
+        {
+            base.InactiveProcess(delta);
+
+            foreach (Behavior Child in ChildBehaviors)
+            {
+                if (!(Child is Event) && Child.Active)
+                {
+                    Child.Process(delta);
+                }
+            }
+        }
+
+    #endregion
+
+
+    #region Durations
+
+        public override void StartDuration(Type durationType)
+        {
+            // Do nothing
+        }
+
+        public override void StopDuration(Type durationType)
+        {
+            // Do nothing
+        }
+
+    #endregion
+
+
+    #region Events
+
+        public override void ExecuteEvent(Type eventType)
+        {
+            if (GetType() == eventType)
+            {
+                Begin();
+            }
+        }
+
+    #endregion
 
 }
