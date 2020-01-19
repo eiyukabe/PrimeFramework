6815e8d7a5fdeefd6784d2ae5473882b2aeb478e
eiyukabe <eiyukabe@gmail.com>
2020-01-19 18:17:22 -0500

    Remove TEST_SCREEN reference from GameScenes.cs.

--- Game/GameScenes/GameScenes.cs
+++ Game/GameScenes/GameScenes.cs
@@ -6,6 +6,5 @@ public static partial class GameScenes
     public const string CREDITS = "res://Game/GameScenes/Credits/Credits.tscn";
     public const string PAUSE_MENU = "res://Game/GameScenes/PauseMenu/PauseMenu.tscn";
     public const string PLAY = "res://Game/GameScenes/Play/Play.tscn";
-    public const string TEST_SCREEN = "res://Game/GameScenes/TestScreen/TestScreen.tscn";
     public const string TITLE_SCREEN = "res://Game/GameScenes/TitleScreen/TitleScreen.tscn";
 }
