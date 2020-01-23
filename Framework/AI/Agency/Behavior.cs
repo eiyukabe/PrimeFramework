1f8300e6dd0004d10e5c582155fb6d1f0592b1c3
eiyukabe <eiyukabe@gmail.com>
2020-01-22 20:43:25 -0500

    Fix bug where agency-less behaviors were not beginning.

--- Framework/AI/Agency/Behavior.cs
+++ Framework/AI/Agency/Behavior.cs
@@ -68,6 +68,7 @@ public abstract class Behavior : PrimeNode
                     if (ParentBehavior == null)
                     {
                         Setup();
+                        Begin();
                     }
                 }
             }
