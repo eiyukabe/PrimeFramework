af83202e3504ebbb3905e8e69b77677194e656f4
Brian <veluzy@gmail.com>
2020-05-27 22:39:48 +0800

    GameScenes.cs: fixed broken paths due to renaming.

--- Game/Prime Scenes/GameScenes.cs
+++ Game/Prime Scenes/GameScenes.cs
@@ -5,7 +5,7 @@ public static partial class PrimeScenes
 {
     public const string CREDITS = "res://Game/Prime Scenes/Credits/Credits.tscn";
     public const string OPTIONS = "res://Game/Prime Scenes/Options/OptionsMenu.tscn";
-    public const string PAUSE_MENU = "res://Game/Prime Scenes/PauseMenu/PauseMenu.tscn";
+    public const string PAUSE_MENU = "res://Game/Prime Scenes/Pause Menu/PauseMenu.tscn";
     public const string PLAY = "res://Game/Prime Scenes/Play/Play.tscn";
-    public const string TITLE_SCREEN = "res://Game/Prime Scenes/TitleScreen/TitleScreen.tscn";
+    public const string TITLE_SCREEN = "res://Game/Prime Scenes/Title Screen/TitleScreen.tscn";
 }
