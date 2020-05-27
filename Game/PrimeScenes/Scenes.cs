b6ff705a6be9ebcd76a23f118085d652254d2a76
Brian <veluzy@gmail.com>
2020-05-28 03:28:21 +0800

    Renamed folders with spaces back to Pascal case because there's a Godot bug that prevents you from using space names in folders with C#...

---
+++ Game/PrimeScenes/Scenes.cs
@@ -0,0 +1,11 @@
+using Godot;
+using System;
+
+public static partial class Scenes
+{
+    public const string CREDITS = "res://Game/PrimeScenes/Credits/Credits.tscn";
+    public const string OPTIONS = "res://Game/PrimeScenes/Options/OptionsMenu.tscn";
+    public const string PAUSE_MENU = "res://Game/PrimeScenes/PauseMenu/PauseMenu.tscn";
+    public const string PLAY = "res://Game/PrimeScenes/Play/Play.tscn";
+    public const string TITLE_SCREEN = "res://Game/PrimeScenes/TitleScreen/TitleScreen.tscn";
+}
